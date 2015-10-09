6daea7d9ca993bd5bf5bb8374d72d96e0688a7da
Kazuki Sakamoto <sakamoto@splhack.org>
2015-10-09 11:23:18 -0700

    Fix skk dict

--- cmigemo-mk.rb
+++ cmigemo-mk.rb
@@ -6,10 +6,6 @@ class CmigemoMk < Formula
 
   depends_on 'nkf' => :build
 
-  def patches
-    DATA
-  end
-
   def install
     ENV.append 'LDFLAGS', '-headerpad_max_install_names'
 
@@ -22,18 +18,3 @@ class CmigemoMk < Formula
     system "make osx-install"
   end
 end
-
-__END__
-diff --git a/dict/dict.mak b/dict/dict.mak
-index 8ea8a66..4f27a97 100644
---- a/dict/dict.mak
-+++ b/dict/dict.mak
-@@ -6,7 +6,7 @@
- 
- DICT 		= migemo-dict
- DICT_BASE	= base-dict
--SKKDIC_BASEURL 	= http://openlab.ring.gr.jp/skk/dic
-+SKKDIC_BASEURL 	= http://web.archive.org/web/20110707131038/http://openlab.ring.gr.jp/skk/dic
- SKKDIC_FILE	= SKK-JISYO.L
- EUCJP_DIR	= euc-jp.d
- UTF8_DIR	= utf-8.d
