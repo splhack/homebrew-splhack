d031d8b3bddf0fe75f2d34a4721ae229604c4c73
Kazuki Sakamoto <sakamoto@splhack.org>
2016-04-04 22:08:38 -0700

    Workaround for SKK dict

--- cmigemo-mk.rb
+++ cmigemo-mk.rb
@@ -6,6 +6,10 @@ class CmigemoMk < Formula
 
   depends_on 'nkf' => :build
 
+  def patches
+    DATA
+  end
+
   def install
     ENV.append 'LDFLAGS', '-headerpad_max_install_names'
 
@@ -18,3 +22,31 @@ class CmigemoMk < Formula
     system "make osx-install"
   end
 end
+
+__END__
+diff --git a/configure b/configure
+index 4480261..0ea4156 100755
+--- a/configure
++++ b/configure
+@@ -30,7 +30,7 @@ done
+ 
+ # Check HTTP access tool
+ if CHECK_COMMAND curl ; then
+-  PROGRAM_HTTP="curl -O"
++  PROGRAM_HTTP="curl -L -O"
+ elif CHECK_COMMAND wget ; then
+   PROGRAM_HTTP="wget"
+ elif CHECK_COMMAND fetch ; then
+diff --git a/dict/dict.mak b/dict/dict.mak
+index 8ea8a66..be17810 100644
+--- a/dict/dict.mak
++++ b/dict/dict.mak
+@@ -6,7 +6,7 @@
+ 
+ DICT 		= migemo-dict
+ DICT_BASE	= base-dict
+-SKKDIC_BASEURL 	= http://openlab.ring.gr.jp/skk/dic
++SKKDIC_BASEURL 	= http://web.archive.org/web/20150917000430/http://openlab.ring.gr.jp/skk/dic
+ SKKDIC_FILE	= SKK-JISYO.L
+ EUCJP_DIR	= euc-jp.d
+ UTF8_DIR	= utf-8.d
